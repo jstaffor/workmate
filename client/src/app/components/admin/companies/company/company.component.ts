24f6babfa7cee430b88657a690181c85cc05fdf4
pstafford <paul.stafford@visual-meta.com>
2019-03-07 17:42:38 +0100

    small update

--- client/src/app/components/admin/companies/company/company.component.ts
+++ client/src/app/components/admin/companies/company/company.component.ts
@@ -1,9 +1,10 @@
 ﻿import { Component, OnInit, Inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Company } from '../../../../models/company';
 import { ENUM_active } from '../../../../models/enums/enum-enable';
+import { CompanyHttp } from '../../../../http/company-http';
 
 
 
@@ -13,19 +14,20 @@ import { ENUM_active } from '../../../../models/enums/enum-enable';
 })
 
 export class CompanyDialogComponent  {
+  company: Company;
   options: FormGroup;
-  submitted = true;  
   actives = Object.keys(ENUM_active).filter(v => isNaN(parseInt(v, 10)));
-
+  submitted = true;  
 
   constructor(
     public dialogRef: MatDialogRef<CompanyDialogComponent>,
+    private companyHttp: CompanyHttp,
     fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: JSON) {
-      let company = data['Company'] as Company;
+      this.company = data['Company'] as Company;     
        this.options = fb.group({
-        name: company.name,
-        active: ENUM_active[company.active]
+        name: new FormControl(this.company.name, [Validators.required]),
+        active: new FormControl(ENUM_active[this.company.active], [Validators.required])
       });
     }
 
@@ -37,7 +39,55 @@ export class CompanyDialogComponent  {
       this.submitted = false;
     }
 
-    save() {
-      alert('hi');
+    save(company: Company) {
+      debugger;
+      if(company.id != undefined){
+        this.add(company);
+      } else {
+        this.update(company);
+      }
+      
     }
+
+    getErrorMessage() {
+      return this.options.get('name').hasError('required') ? 'You must enter a value' : 
+      this.options.get('active').hasError('email') ? 'You must enter a value' :
+      '';
+    }
+
+    add(company: Company): void {
+      if (!name) { return; }
+      this.companyHttp.addCompany(company)
+        .subscribe(success => {
+          debugger;
+          alert('success: ' + success.name);
+        }, error => {
+          debugger;
+          alert('error: ' + error);
+        });
+    }
+
+    update(company: Company): void {
+      if (!name) { return; }
+      this.companyHttp.updateCompany(company)
+        .subscribe(success => {
+          debugger;
+          alert('success: ' + success.name);
+        }, error => {
+          debugger;
+          alert('error: ' + error);
+        });
+    }
+   
+    delete(company: Company): void {
+      this.companyHttp.deleteCompany(company)
+        .subscribe(success => {
+          debugger;
+          alert('success');
+        }, error => {
+          debugger;
+          alert('error');
+        });
+    }
+  
 }
--- client/src/app/http/company-http.ts
+++ client/src/app/http/company-http.ts
@@ -8,12 +8,25 @@ import { catchError, map, tap } from 'rxjs/operators';
 @Injectable()
 export class CompanyHttp {
     constructor(private http: HttpClient) { }
-    
+
+    private company_url = 'http://localhost:8082/admin/company';
+
     getCompanies (): Observable<Company[]> {
         return this.http.get<Company[]>('http://localhost:8082/admin/getCompanies')
-        .pipe(
-            // tap(_ => this.log('fetched companies')),
-            // catchError(this.handleError('getCompanies', []))
-        );
+        .pipe();
+    }
+ 
+    addCompany (company: Company): Observable<Company> {
+        return this.http.post<Company>(this.company_url, company).pipe();
+    }
+    
+    deleteCompany (company: Company | number): Observable<Company> {
+        const id = typeof company === 'number' ? company : company.id;
+        const url = `${this.company_url}/${id}`; 
+        return this.http.delete<Company>(url).pipe();
+    }
+    
+    updateCompany (company: Company): Observable<any> {
+        return this.http.put(this.company_url, company).pipe();
     }
 }
